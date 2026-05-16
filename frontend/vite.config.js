a767a9a07947b3ac2253f59b4de8cf3f75160b94
srimadhavsats <48337401+srimadhavsats@users.noreply.github.com>
2026-05-16 23:51:43 +0530

    chore: implement strict port binding and host rules in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' //
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +8,9 @@ export default defineConfig({
     react(),
     tailwindcss(), // Adds Tailwind directly to the build pipeline
   ],
-})
+  server: {
+    port: 5174,
+    host: "127.0.0.1",
+    strictPort: true,
+  },
+});
